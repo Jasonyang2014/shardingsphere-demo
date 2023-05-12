691600f46ef6f9c327f6e02bfd51249a3ac3966d
Jason AuYeung <305744830@qq.com>
2023-05-12 21:10:39 +0800

    init elastic job

---
+++ elastic-job-demo/src/main/java/com/example/demo/jobs/MyJob.java
@@ -0,0 +1,11 @@
+package com.example.demo.jobs;
+
+import org.apache.shardingsphere.elasticjob.api.ShardingContext;
+import org.apache.shardingsphere.elasticjob.simple.job.SimpleJob;
+
+public class MyJob implements SimpleJob {
+    @Override
+    public void execute(ShardingContext shardingContext) {
+
+    }
+}
